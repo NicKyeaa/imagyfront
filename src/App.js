a8af490bf319ac96d0e4b707df6e8ff06f49c970
NicKyeaa <tin.jurcevic@gmail.com>
2022-03-23 21:47:39 +0100

    Added Card Component from MUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ResponsiveAppBar from "./Components/ResponsiveAppbar";
+import PictureCard from "./Components/PictureCard";
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
+      <PictureCard />
     </div>
   );
 }
